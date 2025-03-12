36a2b50c22095e7d61e02c0499021ba7fed89c52
Holly Black <hblack@highpoint.edu>
2025-03-12 03:13:39 -0400

    Groups Table

--- mc_basic_code/mc_database.sql
+++ mc_basic_code/mc_database.sql
@@ -21,12 +21,12 @@ CREATE TABLE groups (
     description TEXT
 );
 
-CREATE TABEL user_groups (
+CREATE TABLE user_groups (
     user_id INT,
     group_id INT,
-    PRIMARY KWY (uaer_id, group_id),
-    FOREIGN KEY (user_id) REFERENCES user(id),
-    FOREIGN KEY (group_id) REFERENCES groups(id)
+    PRIMARY KEY (user_id, group_id),
+    FOREIGN KEY (user_id) REFERENCES user(User_id),
+    FOREIGN KEY (group_id) REFERENCES groups(group_id)
 );
 
 CREATE TABLE workouts (
