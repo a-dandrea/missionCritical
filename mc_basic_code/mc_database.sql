00693f895fd1cd559302ad7cc61cf343266ded1d
Holly Black <hblack@highpoint.edu>
2025-03-12 03:15:20 -0400

    Groups Table

--- mc_basic_code/mc_database.sql
+++ mc_basic_code/mc_database.sql
@@ -25,7 +25,7 @@ CREATE TABLE user_groups (
     user_id INT,
     group_id INT,
     PRIMARY KEY (user_id, group_id),
-    FOREIGN KEY (user_id) REFERENCES user(User_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
     FOREIGN KEY (group_id) REFERENCES groups(group_id)
 );
 
