b2f8843ebf02938e1cf8f86122449a03ddcab257
Alexa Dandrea <alexadandrea818@gmail.com>
2025-03-12 03:18:53 -0400

    update database

--- mc_basic_code/mc_database.sql
+++ mc_basic_code/mc_database.sql
@@ -24,7 +24,7 @@ CREATE TABLE groups (
 CREATE TABLE user_groups (
     user_id INT,
     group_id INT,
-    FOREIGN KEY (user_id) REFERENCES useris(user_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (group_id) REFERENCES groups(group_id)
 );
 
