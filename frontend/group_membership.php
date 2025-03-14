d9ecb014dfb8e643a7d3457dacd4f630b88cd3a3
Holly Black <hblack@highpoint.edu>
2025-03-14 15:39:56 -0400

    updated

--- frontend/group_membership.php
+++ frontend/group_membership.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 $dsn = 'mysql:host=joecool.highpoint.edu;dbname=csc4710_S25_missioncritical';
 $username = 'ejerrier';
 $password = '1788128';
@@ -134,5 +130,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['join_group'])) {
         <script>
             alert("<?php echo $popupMessage; ?>");
         </script>
-    <?
+    <?php endif; ?>
+</body>
+</html>
 
