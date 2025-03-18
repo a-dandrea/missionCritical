d6249a729fab2ae2386d79e4cf4c6a46491e5db0
Alexa Dandrea <alexadandrea818@gmail.com>
2025-03-18 15:49:24 -0400

    revert personalinfo.php

--- frontend/personalinfo.php
+++ frontend/personalinfo.php
@@ -62,8 +62,8 @@ try {
         </form>
     </div>
 
-    
-    <script src="assets/progress.js"></script>  <!-- Link to progress.js -->
+    <script src="assets/info.js"></script> <!-- Link to script.js -->
+    <!--<script src="assets/progress.js"></script> --> <!-- Link to progress.js -->
 </body>
 </html>
 
