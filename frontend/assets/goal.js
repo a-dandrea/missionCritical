167fd803af09a1b639fc5eaa63485f4cf948aa8c
Alexa Dandrea <alexadandrea818@gmail.com>
2025-03-19 10:37:05 -0400

    no changes

--- frontend/assets/goal.js
+++ frontend/assets/goal.js
@@ -8,16 +8,16 @@ document.getElementById("update-goal-form").addEventListener("submit", function(
        method: 'POST',
        body: formData
    })
-   .then(response => response.json())
-   .then(data => {
-       alert(data.message);  // Show success/error message
-       if(data.message === "Goal updated successfully!"){
-         //add any needed logic after a successful database update.
-       }
-       document.getElementById("update-goal-form").reset();  // Reset the form
-   })
-   .catch(error => {
-      console.error('Error:', error);
-      alert('An error occurred while updating the goal.');
-  });
+      .then(response => response.json())
+      .then(data => {
+         alert(data.message);  // Show success/error message
+         if(data.message === "Goal updated successfully!"){
+            //add any needed logic after a successful database update.
+         }
+         document.getElementById("update-goal-form").reset();  // Reset the form
+      })
+      .catch(error => {
+         console.error('Error:', error);
+         alert('An error occurred while updating the goal.');
+      });
 });
