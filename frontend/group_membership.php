f35feb12a1e2ef4d60af92202ef606a2f6a3d5ef
Holly Black <hblack@highpoint.edu>
2025-03-21 15:14:46 -0400

    group page updates

--- frontend/group_membership.php
+++ frontend/group_membership.php
@@ -26,8 +26,7 @@
               $db->beginTransaction();
 
               // Insert new group
-              $$insertGroupSql = "INSERT INTO groups (username, type) VALUES (:username, :type)"; stmt = $db->prepare($insertGroupSql);
-              $stmt->bindParam(':username', $username);
+              $$insertGroupSql = "INSERT INTO groups (username, group_type) VALUES (:username, :group_type)"; stmt->bindParam(':username', $username);
               $stmt->bindParam(':group_type', $group_type);
               $stmt->bindParam(':parental_control', $parental_control, PDO::PARAM_INT);
               $stmt->execute();
