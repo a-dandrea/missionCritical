24cb9de4157db494a59efdaa97d5a7b11f7697eb
cblack <cblack@linus.highpoint.edu>
2025-04-16 15:03:04 -0400

    IMAGE

--- frontend/workout.php
+++ frontend/workout.php
@@ -47,7 +47,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log Your Workout</title>
+    <title>Workout</title>
     <link rel="icon" href="images/rocket-icon.png" type="image/png">
     <link rel="stylesheet" href="style.css">
 </head>
