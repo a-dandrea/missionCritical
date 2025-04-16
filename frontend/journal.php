82c94a5eca0155ff1354f65a608242958baffb7b
cblack <cblack@linus.highpoint.edu>
2025-04-16 15:01:26 -0400

    IMAGE

--- frontend/journal.php
+++ frontend/journal.php
@@ -116,7 +116,7 @@ $stmt->closeCursor();
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Dashboard</title>
+   <title>Journal</title>
    <link rel="icon" href="images/astronaut.png">
    <link rel="stylesheet" href="style.css"> 
 </head>
